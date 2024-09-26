553ab60b7233492bba4f080237c7dda4e258a706
pengyi <ryanpoy@gmail.com>
2024-09-26 21:23:47 +0800

    refactoring

--- ip/internal/utils.go
+++ ip/internal/utils.go
@@ -10,21 +10,6 @@ type Int128 struct {
 	L uint64
 }
 
-func FromIpv6(v6 string) *Int128 {
-	ip := net.ParseIP(v6)
-	if ip == nil {
-		return nil
-	}
-	v := ip.To16()
-	if v == nil {
-		return nil
-	}
-	return &Int128{
-		H: binary.BigEndian.Uint64(v[0:8]),
-		L: binary.BigEndian.Uint64(v[8:16]),
-	}
-}
-
 func (i *Int128) Cmp(j *Int128) int {
 	if i.H > j.H {
 		return 1
@@ -39,4 +24,19 @@ func (i *Int128) Cmp(j *Int128) int {
 		return -1
 	}
 	return 0
+}
+
+func NewI128From(ipStr string) *Int128 {
+	ip := net.ParseIP(ipStr)
+	if ip == nil {
+		return nil
+	}
+	v := ip.To16()
+	if v == nil {
+		return nil
+	}
+	return &Int128{
+		H: binary.BigEndian.Uint64(v[0:8]),
+		L: binary.BigEndian.Uint64(v[8:16]),
+	}
 }
