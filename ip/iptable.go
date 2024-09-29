ad5a041809f7d2a17663b6145fc904ede5d0aa5d
pengyi <ryanpoy@gmail.com>
2024-09-29 14:27:13 +0800

    performance optimization

--- ip/iptable.go
+++ ip/iptable.go
@@ -2,6 +2,7 @@ package ip
 
 import (
 	"bufio"
+	"encoding/binary"
 	"gommon/extends/array"
 	"gommon/ip/consts"
 	"net"
@@ -15,8 +16,8 @@ func isV4(ipStr string) bool {
 }
 
 type IPTable struct {
-	v4s       IPRanges
-	v6s       IPRanges
+	v4s       V4Ranges
+	v6s       V6Ranges
 	countries *array.Array
 	isps      *array.Array
 	provs     *array.Array
@@ -24,15 +25,24 @@ type IPTable struct {
 	numbers   *array.Array
 }
 
-func (t *IPTable) AddV4(x *IPRange) {
+func (t *IPTable) AddV4(x *V4Range) {
 	t.v4s = append(t.v4s, x)
 }
 
-func (t *IPTable) AddV6(x *IPRange) {
+func (t *IPTable) AddV6(x *V6Range) {
 	t.v6s = append(t.v6s, x)
 }
 
-func (t *IPTable) StringOf(ipRange *IPRange) string {
+func (t *IPTable) StringOfV4(ipRange *V4Range) string {
+	return ipRange.StartStr + "|" +
+		ipRange.EndStr + "|" +
+		t.countries.Get(ipRange.CountryIdx) + "|" +
+		t.isps.Get(ipRange.IspIdx) + "|" +
+		t.provs.Get(ipRange.ProvIdx) + "|" +
+		t.cities.Get(ipRange.CityIdx) + "|" +
+		t.numbers.Get(ipRange.NumberIdx)
+}
+func (t *IPTable) StringOfV6(ipRange *V6Range) string {
 	return ipRange.StartStr + "|" +
 		ipRange.EndStr + "|" +
 		t.countries.Get(ipRange.CountryIdx) + "|" +
@@ -54,38 +64,40 @@ func (t *IPTable) AreaOf(ipRange *IPRange) map[string]string {
 	}
 }
 
-func (t *IPTable) Search(ipStr string) *IPRange {
-	ip := net.ParseIP(ipStr)
+func (t *IPTable) SearchV4(ipv4 string) *V4Range {
+	ip := net.ParseIP(ipv4)
 	if ip == nil {
 		return nil
 	}
-	if strings.Contains(ipStr, ".") {
-		return t.SearchV4(ip)
-	} else {
-		return t.SearchV6(ip)
-	}
-}
-
-func (t *IPTable) SearchV4(ipv4 net.IP) *IPRange {
-	if ipv4 = ipv4.To4(); ipv4 == nil {
-		return nil
-	}
-	return t.search(ipv4, t.v4s)
-}
-
-func (t *IPTable) SearchV6(ipv6 net.IP) *IPRange {
-	if ipv6 = ipv6.To16(); ipv6 == nil {
-		return nil
-	}
-	return t.search(ipv6, t.v6s)
-}
-
-func (t *IPTable) search(ip net.IP, ranges IPRanges) *IPRange {
+	ip = ip.To4()
 	if ip == nil {
 		return nil
 	}
+	ranges := t.v4s
 	idx := sort.Search(len(ranges), func(i int) bool {
-		return cmp(ranges[i].low, ip) == 1 || cmp(ranges[i].high, ip) != -1
+		return ranges[i].Gte(ip)
+		//return cmp(ranges[i].Low, ip) == 1 || cmp(ranges[i].High, ip) != -1
+	})
+
+	if idx < len(ranges) && ranges[idx].Contains(ip) {
+		return ranges[idx]
+	}
+	return nil
+}
+
+func (t *IPTable) SearchV6(ipv6 string) *V6Range {
+	ip := net.ParseIP(ipv6)
+	if ip == nil {
+		return nil
+	}
+	ip = ip.To16()
+	if ip == nil {
+		return nil
+	}
+	ranges := t.v6s
+	idx := sort.Search(len(ranges), func(i int) bool {
+		return ranges[i].Gte(ip)
+		//return cmp(ranges[i].Low, ip) == 1 || cmp(ranges[i].High, ip) != -1
 	})
 
 	if idx < len(ranges) && ranges[idx].Contains(ip) {
@@ -98,7 +110,7 @@ func (t *IPTable) sortAndUniq() {
 	sort.Sort(&t.v4s)
 	sort.Sort(&t.v6s)
 
-	uniqV4s, uniqV6s := make(IPRanges, 0), make(IPRanges, 0)
+	uniqV4s, uniqV6s := make(V4Ranges, 0), make(V6Ranges, 0)
 	if len(t.v4s) > 0 {
 		uniqV4s = append(uniqV4s, t.v4s[0])
 		for i := 1; i < len(t.v4s); i++ {
@@ -121,8 +133,8 @@ func (t *IPTable) sortAndUniq() {
 func NewIPTable(fpaths ...string) (*IPTable, error) {
 	var err error
 	table := &IPTable{
-		v4s:       make(IPRanges, 0),
-		v6s:       make(IPRanges, 0),
+		v4s:       make(V4Ranges, 0),
+		v6s:       make(V6Ranges, 0),
 		countries: array.New(),
 		isps:      array.New(),
 		provs:     array.New(),
@@ -174,28 +186,33 @@ func initFromFile(fpath string, table *IPTable) (*IPTable, error) {
 			low, high = high, low
 		}
 		if isV4 {
-			table.AddV4(&IPRange{
-				low:        low,
-				high:       high,
-				StartStr:   parts[0],
-				EndStr:     parts[1],
-				CountryIdx: table.countries.Append(parts[2]),
-				IspIdx:     table.isps.Append(parts[3]),
-				ProvIdx:    table.provs.Append(parts[4]),
-				CityIdx:    table.cities.Append(parts[5]),
-				NumberIdx:  table.numbers.Append(parts[6]),
+			v1, v2 := binary.BigEndian.Uint32(low), binary.BigEndian.Uint32(high)
+			table.AddV4(&V4Range{
+				Low:  &v1,
+				High: &v2,
+				IPRange: IPRange{
+					StartStr:   parts[0],
+					EndStr:     parts[1],
+					CountryIdx: table.countries.Append(parts[2]),
+					IspIdx:     table.isps.Append(parts[3]),
+					ProvIdx:    table.provs.Append(parts[4]),
+					CityIdx:    table.cities.Append(parts[5]),
+					NumberIdx:  table.numbers.Append(parts[6]),
+				},
 			})
 		} else {
-			table.AddV6(&IPRange{
-				low:        low,
-				high:       high,
-				StartStr:   parts[0],
-				EndStr:     parts[1],
-				CountryIdx: table.countries.Append(parts[2]),
-				IspIdx:     table.isps.Append(parts[3]),
-				ProvIdx:    table.provs.Append(parts[4]),
-				CityIdx:    table.cities.Append(parts[5]),
-				NumberIdx:  table.numbers.Append(parts[6]),
+			table.AddV6(&V6Range{
+				Low:  low,
+				High: high,
+				IPRange: IPRange{
+					StartStr:   parts[0],
+					EndStr:     parts[1],
+					CountryIdx: table.countries.Append(parts[2]),
+					IspIdx:     table.isps.Append(parts[3]),
+					ProvIdx:    table.provs.Append(parts[4]),
+					CityIdx:    table.cities.Append(parts[5]),
+					NumberIdx:  table.numbers.Append(parts[6]),
+				},
 			})
 		}
 	}
